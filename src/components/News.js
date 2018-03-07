b709b1d646fec9b5cb94e8f477c506032da5b6ae
Vincent Howard <vincenguyenhoward@gmail.com>
2018-03-07 11:53:09 -0700

    started on news section html css

--- src/components/News.js
+++ src/components/News.js
@@ -4,10 +4,146 @@ import { Link } from "react-router-dom";
 export default class News extends Component {
   render() {
     return (
-<div>
-  <div className="content">
+<div className="content">
 
+  <h2 className='news-header'>News</h2>
+
+  <div className='news-container'>
+    <div className='news-post'>
+      <a href='https://medium.com/@Francesco_AI/the-convergence-of-ai-and-blockchain-whats-the-deal-60c618e3accc'>
+        <img className='news-img' src="https://cdn-images-1.medium.com/max/2000/1*xJ9uihFO6wBW5RfyEVbuEQ.jpeg" alt="news" />
+      </a>
+      <a href='https://medium.com/@Francesco_AI/the-convergence-of-ai-and-blockchain-whats-the-deal-60c618e3accc'>
+        <div className='news-detail-container'>
+          <div className='news-container-padding'>
+            <h2 className='news-title'>The convergence of AI and Blockchain: what’s the deal?</h2>
+            <div className='news-caption'>Why a decentralized intelligence may affect our future</div>
+            <div className='news-caption'>is undeniable that AI and blockchain are two of the major technologies that are catalyzing the pace of innovation
+              and introducing radical shifts in every industry. Each technology has its own degree of technical complexity
+              as well as business implications but the joint use of the two may be able to redesign the entire...</div>
+            <div className='learn-more-container'>
+              <div className='news-learn-more'>Learn More</div>
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
   </div>
+
+
+  <div className='news-container'>
+    <div className='news-post'>
+      <a href='https://cointelegraph.com/news/neureal-bringing-artificial-intelligence-to-the-people?utm_content=bufferae0b9&utm_medium=social&utm_source=facebook.com&utm_campaign=buffer'>
+        <img className='news-img' src="https://cointelegraph.com/images/725_Ly9jb2ludGVsZWdyYXBoLmNvbS9zdG9yYWdlL3VwbG9hZHMvdmlldy9kM2Q3YzgzNzIxZjdmODNiMDMxOTNlZWI2Y2JjMWUzMS5wbmc=.jpg"
+          alt="news" />
+      </a>
+      <a href='https://cointelegraph.com/news/neureal-bringing-artificial-intelligence-to-the-people?utm_content=bufferae0b9&utm_medium=social&utm_source=facebook.com&utm_campaign=buffer'>
+        <div className='news-detail-container'>
+          <div className='news-container-padding'>
+            <h2 className='news-title'>Иeureal: Bringing Artificial Intelligence to the People</h2>
+            <div className='news-caption'></div>
+            <div className='news-caption'>Иeureal is the first decentralized open-source protocol designed to produce a distributed artificial intelligence
+              (AI) architecture that is incentivized, maintained and housed within a cryptocurrency. The project is an infrastructure
+              for predictive algorithms, like an ongoing kaggle. Creator Wil Brown started the project with the idea that
+              AI could be hugely impacted with the help of the technology behind Bitcoin, to pursue his “mad scientist” dream
+              of creating an AI system....</div>
+            <div className='learn-more-container'>
+              <div className='news-learn-more'>Learn More</div>
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+  </div>
+
+  <div className='news-container'>
+    <div className='news-post'>
+      <a href='https://www.newgenapps.com/blog/future-of-ico-impact-of-initial-coin-offering'>
+        <img className='news-img' src="https://www.newgenapps.com/hs-fs/hubfs/bitcoin.jpg?width=1196&height=598&name=bitcoin.jpg"
+          alt="news" />
+      </a>
+      <a href='https://www.newgenapps.com/blog/future-of-ico-impact-of-initial-coin-offering'>
+        <div className='news-detail-container'>
+          <div className='news-container-padding'>
+            <h2 className='news-title'>11 Experts on the Future of ICO & its Impact on Financial Markets</h2>
+            <div className='news-caption'></div>
+            <div className='news-caption'>ICO i.e. Initial Coin Offering is the new wave of fundraising where ideators ditch the complete hassle of the lengthy funding process and raise capital via blockchain technology. With a simple plan of action on a white paper, a website and basic skills in coding anybody can raise tremendous capital from the cryptocurrency market.....</div>
+            <div className='learn-more-container'>
+              <div className='news-learn-more'>Learn More</div>
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+  </div>
+
+  <div className='news-container'>
+    <div className='news-post'>
+      <a href='https://medium.com/@chain_explainer/blockchain-insider-jen-greyson-2610af01c709'>
+        <img className='news-img' src="https://cdn-images-1.medium.com/max/1600/1*pYXvf8puA9rCQunqjBJlWw.jpeg"
+          alt="news" />
+      </a>
+      <a href='https://medium.com/@chain_explainer/blockchain-insider-jen-greyson-2610af01c709'>
+        <div className='news-detail-container'>
+          <div className='news-container-padding'>
+            <h2 className='news-title'>Blockchain Insider: Jen Greyson</h2>
+            <div className='news-caption'></div>
+            <div className='news-caption'>Recently named one of the Top 8 Women in Crypto, Jen Greyson of the Neureal Network and CEO of Powered by Neureal brings decades of executive leadership to the blockchain/crypto space. Her adamant stances on empowering women, lifting global issues, and educating over regulating, set her team at Neureal apart in the frenzy that is the current crypto space. With a project tackling artificial intelligence, big data, and cryptocurrency, she’ll be one to watch as she bridges some of the biggest nascent technologies.....</div>
+            <div className='learn-more-container'>
+              <div className='news-learn-more'>Learn More</div>
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+  </div>
+
+  <div className='news-container'>
+    <div className='news-post'>
+      <a href='https://medium.com/@JenGreyson/blockchain-5-years-from-now-cda989c3048b'>
+        <img className='news-img'  src="https://image.ibb.co/k2YHTS/neureal_background_final.jpg"
+          alt="news" />
+      </a>
+      <a href='https://medium.com/@JenGreyson/blockchain-5-years-from-now-cda989c3048b'>
+        <div className='news-detail-container'>
+          <div className='news-container-padding'>
+            <h2 className='news-title'>Blockchain. 5 Years From Now.</h2>
+            <div className='news-caption'></div>
+            <div className='news-caption'>I had an interviewer ask me where I thought Ethereum was going to be in 5 years.
+
+Five years. That’s a lifetime in blockchain.
+
+Look what we’ve seen in the last five MONTHS.....</div>
+            <div className='learn-more-container'>
+              <div className='news-learn-more'>Learn More</div>
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+  </div>
+
+  <div className='news-container'>
+    <div className='news-post'>
+      <a href='hhttp://usaweekly.com/2018/01/interview-with-jen-greyson-ceo-at-neureal/'>
+        <img className='news-img'  src="https://www.strongholdcybersecurity.com/wp-content/uploads/2018/01/usa-weekly.jpg"
+          alt="news" />
+      </a>
+      <a href='hhttp://usaweekly.com/2018/01/interview-with-jen-greyson-ceo-at-neureal/'>
+        <div className='news-detail-container'>
+          <div className='news-container-padding'>
+            <h2 className='news-title'>Interview with Jen Greyson, CEO at Neureal</h2>
+            <div className='news-caption'></div>
+            <div className='news-caption'>Starting a business is a big achievement for many entrepreneurs, but maintaining one is the larger challenge. There are many standard challenges that face every business whether they are large or small. It is not easy running a company, especially in a fast-paced, ever-changing business world. Technology advances, new hiring strategies, and now, political changes coming with the new administration, all add to the existing business challenges that entrepreneurs, business owners, and executives have to deal with...</div>
+            <div className='learn-more-container'>
+              <div className='news-learn-more'>Learn More</div>
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+  </div>
+
 </div>
     )
   }
