821bf7f4098f72f88cd11b3408ba83fe6eb091a9
Vincent Howard <vincenguyenhoward@gmail.com>
2018-03-07 15:11:10 -0700

    news and podcasts finished, starting videos

--- src/components/News.js
+++ src/components/News.js
@@ -5,7 +5,8 @@ export default class News extends Component {
   render() {
     return (
 <div className="content">
-
+<div className="podcast-news-flex">
+<div className='news-column'>
   <h2 className='news-header'>News</h2>
 
   <div className='news-container'>
@@ -18,9 +19,8 @@ export default class News extends Component {
           <div className='news-container-padding'>
             <h2 className='news-title'>The convergence of AI and Blockchain: what’s the deal?</h2>
             <div className='news-caption'>Why a decentralized intelligence may affect our future</div>
-            <div className='news-caption'>is undeniable that AI and blockchain are two of the major technologies that are catalyzing the pace of innovation
-              and introducing radical shifts in every industry. Each technology has its own degree of technical complexity
-              as well as business implications but the joint use of the two may be able to redesign the entire...</div>
+            <div className='news-caption'>It is undeniable that AI and blockchain are two of the major technologies that are catalyzing the pace of innovation
+              and introducing radical shifts in every industry. Each technology has its...</div>
             <div className='learn-more-container'>
               <div className='news-learn-more'>Learn More</div>
             </div>
@@ -43,10 +43,7 @@ export default class News extends Component {
             <h2 className='news-title'>Иeureal: Bringing Artificial Intelligence to the People</h2>
             <div className='news-caption'></div>
             <div className='news-caption'>Иeureal is the first decentralized open-source protocol designed to produce a distributed artificial intelligence
-              (AI) architecture that is incentivized, maintained and housed within a cryptocurrency. The project is an infrastructure
-              for predictive algorithms, like an ongoing kaggle. Creator Wil Brown started the project with the idea that
-              AI could be hugely impacted with the help of the technology behind Bitcoin, to pursue his “mad scientist” dream
-              of creating an AI system....</div>
+              (AI) architecture that is incentivized, maintained and housed...</div>
             <div className='learn-more-container'>
               <div className='news-learn-more'>Learn More</div>
             </div>
@@ -67,7 +64,8 @@ export default class News extends Component {
           <div className='news-container-padding'>
             <h2 className='news-title'>11 Experts on the Future of ICO & its Impact on Financial Markets</h2>
             <div className='news-caption'></div>
-            <div className='news-caption'>ICO i.e. Initial Coin Offering is the new wave of fundraising where ideators ditch the complete hassle of the lengthy funding process and raise capital via blockchain technology. With a simple plan of action on a white paper, a website and basic skills in coding anybody can raise tremendous capital from the cryptocurrency market.....</div>
+            <div className='news-caption'>ICO i.e. Initial Coin Offering is the new wave of fundraising where ideators ditch the complete hassle of the
+              lengthy funding process and raise capital via blockchain technology...</div>
             <div className='learn-more-container'>
               <div className='news-learn-more'>Learn More</div>
             </div>
@@ -80,15 +78,15 @@ export default class News extends Component {
   <div className='news-container'>
     <div className='news-post'>
       <a href='https://medium.com/@chain_explainer/blockchain-insider-jen-greyson-2610af01c709'>
-        <img className='news-img' src="https://cdn-images-1.medium.com/max/1600/1*pYXvf8puA9rCQunqjBJlWw.jpeg"
-          alt="news" />
+        <img className='news-img' src="https://cdn-images-1.medium.com/max/1600/1*pYXvf8puA9rCQunqjBJlWw.jpeg" alt="news" />
       </a>
       <a href='https://medium.com/@chain_explainer/blockchain-insider-jen-greyson-2610af01c709'>
         <div className='news-detail-container'>
           <div className='news-container-padding'>
             <h2 className='news-title'>Blockchain Insider: Jen Greyson</h2>
             <div className='news-caption'></div>
-            <div className='news-caption'>Recently named one of the Top 8 Women in Crypto, Jen Greyson of the Neureal Network and CEO of Powered by Neureal brings decades of executive leadership to the blockchain/crypto space. Her adamant stances on empowering women, lifting global issues, and educating over regulating, set her team at Neureal apart in the frenzy that is the current crypto space. With a project tackling artificial intelligence, big data, and cryptocurrency, she’ll be one to watch as she bridges some of the biggest nascent technologies.....</div>
+            <div className='news-caption'>Recently named one of the Top 8 Women in Crypto, Jen Greyson of the Neureal Network and CEO of Powered by Neureal
+              brings decades of executive leadership to the blockchain/crypto...</div>
             <div className='learn-more-container'>
               <div className='news-learn-more'>Learn More</div>
             </div>
@@ -101,19 +99,15 @@ export default class News extends Component {
   <div className='news-container'>
     <div className='news-post'>
       <a href='https://medium.com/@JenGreyson/blockchain-5-years-from-now-cda989c3048b'>
-        <img className='news-img'  src="https://image.ibb.co/k2YHTS/neureal_background_final.jpg"
-          alt="news" />
+        <img className='news-img' src="https://image.ibb.co/k2YHTS/neureal_background_final.jpg" alt="news" />
       </a>
       <a href='https://medium.com/@JenGreyson/blockchain-5-years-from-now-cda989c3048b'>
         <div className='news-detail-container'>
           <div className='news-container-padding'>
             <h2 className='news-title'>Blockchain. 5 Years From Now.</h2>
             <div className='news-caption'></div>
-            <div className='news-caption'>I had an interviewer ask me where I thought Ethereum was going to be in 5 years.
-
-Five years. That’s a lifetime in blockchain.
-
-Look what we’ve seen in the last five MONTHS.....</div>
+            <div className='news-caption'>I had an interviewer ask me where I thought Ethereum was going to be in 5 years. Five years. That’s a lifetime
+              in blockchain. Look what we’ve seen in the last five MONTHS....</div>
             <div className='learn-more-container'>
               <div className='news-learn-more'>Learn More</div>
             </div>
@@ -126,15 +120,16 @@ Look what we’ve seen in the last five MONTHS.....</div>
   <div className='news-container'>
     <div className='news-post'>
       <a href='hhttp://usaweekly.com/2018/01/interview-with-jen-greyson-ceo-at-neureal/'>
-        <img className='news-img'  src="https://www.strongholdcybersecurity.com/wp-content/uploads/2018/01/usa-weekly.jpg"
-          alt="news" />
+        <img className='news-img' src="https://www.strongholdcybersecurity.com/wp-content/uploads/2018/01/usa-weekly.jpg" alt="news"
+        />
       </a>
       <a href='hhttp://usaweekly.com/2018/01/interview-with-jen-greyson-ceo-at-neureal/'>
         <div className='news-detail-container'>
           <div className='news-container-padding'>
             <h2 className='news-title'>Interview with Jen Greyson, CEO at Neureal</h2>
             <div className='news-caption'></div>
-            <div className='news-caption'>Starting a business is a big achievement for many entrepreneurs, but maintaining one is the larger challenge. There are many standard challenges that face every business whether they are large or small. It is not easy running a company, especially in a fast-paced, ever-changing business world. Technology advances, new hiring strategies, and now, political changes coming with the new administration, all add to the existing business challenges that entrepreneurs, business owners, and executives have to deal with...</div>
+            <div className='news-caption'>Starting a business is a big achievement for many entrepreneurs, but maintaining one is the larger challenge.
+              There are many standard challenges that face every business whether they...</div>
             <div className='learn-more-container'>
               <div className='news-learn-more'>Learn More</div>
             </div>
@@ -144,6 +139,89 @@ Look what we’ve seen in the last five MONTHS.....</div>
     </div>
   </div>
 
+  </div>
+
+  <div className='podcast-column'>
+
+  <h2 className='podcast-header'>Podcasts</h2>
+
+<div className='podcast-container'>
+  <div className='podcast-post'>
+    <a href='https://vimeo.com/227450398'>
+      <div className='news-detail-container'>
+        <div className='news-container-padding'>
+          <div className='podcast-author'>Ernest Hancock</div>
+          <div className='podcast-detail-flex'><a className='headphone ion-headphone' /><div className='podcast-title'>Hour 3</div></div>
+          <div className='learn-more-container'>
+          </div>
+        </div>
+      </div>
+    </a>
+  </div>
+</div>
+
+<div className='podcast-container'>
+  <div className='podcast-post'>
+    <a href='https://l.facebook.com/l.php?u=https%3A%2F%2Fbuff.ly%2F2DtDQym&h=ATPm5b78akZ7GXRdgF_PLrTXA6lbbW1wWlye2KSmX4OMv46h2jlSD9AuMAsn-Fd4WQGijhFFKjvwR-rWzkIsDM-vBluNkK-UGNnRnHwwQjjByLf4hmSJMS5MMEpgCBopvmWcWFecoLyhPpZyKM-_HWcAsN6f5c_0WTi0k9fy0-uEiTf5cIoo4aGWwyKbQtqsPVpBwvUUVA11JnR9olQ7l8cQ2sagq8dAUkSYVSsOQCyZHm0VBembhAKzZAReuBlIqqtfKYItIJ5ivNWlpUoouucPHGfeOcNMHSeLsgE'>
+      <div className='news-detail-container'>
+        <div className='news-container-padding'>
+          <div className='podcast-author'>Brandon Reese & Daniel Blum</div>
+          <div className='podcast-detail-flex'><a className='headphone ion-headphone' /><div className='podcast-title'>On The Bus Podcast</div></div>
+          <div className='learn-more-container'>
+          </div>
+        </div>
+      </div>
+    </a>
+  </div>
+</div>
+
+<div className='podcast-container'>
+  <div className='podcast-post'>
+    <a href='http://alwaysthejourney.com/2017/12/08/always-the-journey-podcast-episode-no-2/'>
+      <div className='news-detail-container'>
+        <div className='news-container-padding'>
+          <div className='podcast-author'>Jason Woodland</div>
+          <div className='podcast-detail-flex'><a className='headphone ion-headphone' /><div className='podcast-title'>Always the Journey Podcast Episode No. 2</div></div>
+          <div className='learn-more-container'>
+          </div>
+        </div>
+      </div>
+    </a>
+  </div>
+</div>
+
+<div className='podcast-container'>
+  <div className='podcast-post'>
+    <a href='http://freecitiespodcast.libsyn.com/decentral-hub-and-d10e-recap'>
+      <div className='news-detail-container'>
+        <div className='news-container-padding'>
+          <div className='podcast-author'>Anthoney</div>
+          <div className='podcast-detail-flex'><a className='headphone ion-headphone' /><div className='podcast-title'>DeCentral Hub and D10e ReCap</div></div>
+          <div className='learn-more-container'>
+          </div>
+        </div>
+      </div>
+    </a>
+  </div>
+</div>
+
+<div className='podcast-container'>
+  <div className='podcast-post'>
+    <a href='https://www.youtube.com/watch?v=tZ2XKUoNtyc'>
+      <div className='news-detail-container'>
+        <div className='news-container-padding'>
+          <div className='podcast-author'>Future Tech Podcast</div>
+          <div className='podcast-detail-flex'><a className='headphone ion-headphone' /><div className='podcast-title'>Neureal Open-Source, Peer-to-Peer Pr</div></div>
+          <div className='learn-more-container'>
+          </div>
+        </div>
+      </div>
+    </a>
+  </div>
+</div>
+
+</div>
+</div>
 </div>
     )
   }
