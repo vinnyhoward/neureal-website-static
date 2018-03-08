bdf26a6cfeaf6169199911d84262cea090363212
Vincent Howard <vincenguyenhoward@gmail.com>
2018-03-07 17:25:10 -0700

    added static.config

--- static.config.js
+++ static.config.js
@@ -1,14 +1,46 @@
 // Though this file is optional, there are SO MANY COOL THINGS you can do here.
 // Read the docs at https://github.com/nozzle/react-static/blob/master/README.md to learn more!
+import React, { Component } from 'react';
+
 export default {
-  // Document: ({ Html, Head, Body, children, siteData, renderMeta, link }) => (
-  //   <Html lang="en-US">
-  //     <Head>
-  //       <meta charSet="UTF-8" />
-  //       <meta name="viewport" content="width=device-width, initial-scale=1" />
-  //       <link rel="shortcut icon" href="https://s13.postimg.org/wow7q9jjr/favicon.png" type="favicon/ico" />
-  //     </Head>
-  //     <Body>{children}</Body>
-  //   </Html>
-  // ),
+getRoutes: async () => {
+  return [
+    {
+      path: '/',
+    },
+    {
+      path: '/roadmap',
+    },
+    {
+      path:  '/blog-1'
+    },
+    {
+      path: '/team'
+    },
+    {
+      path: '/news'
+    },
+  ]
+},
+Html: class CustomHtml extends Component {
+  render() {
+    const {Html, Head, Body, children } = this.this.props
+
+    // const sheet = new ServerStyleShoot()
+    // const newChildren = sheet.collectStyles(children)
+    // const styleTags = sheet.getStyleElement()
+
+    return (
+      <Html>
+        <Head>
+          <meta name='viewport' content='width=device-width, initial-scale=1' />
+            {/* {styleTags}  */}
+        </Head>
+        <Body>
+          {children}
+        </Body>
+      </Html>
+    )
+  }
+},
 }
