1d2960829381b35497d0295d0490b9741344585c
Vincent Howard <vincenguyenhoward@gmail.com>
2018-03-12 13:44:55 -0600

    added social media icons too footer, enlarged and replaced social media icons on mobile sidebar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,6 +18,17 @@ export default class Footer extends Component { render() { return (
         </div>
       </div>
     </div>
+    <div className="social-icon-footer">
+      <a className='git-footer ion-social-github' href="https://github.com/neureal" target="_blank"></a>
+      <a className='fb-footer ion-social-facebook' href="https://github.com/neureal" target="_blank"></a>
+      <a className='rdt-footer ion-social-reddit' href="https://www.reddit.com/r/NeuralNetwork/" target="_blank"></a>
+      <a className='twi-footer ion-social-twitter' href="https://twitter.com/neurealai" target="_blank"></a>
+      <a className='lnk-footer ion-social-linkedin' href="https://www.linkedin.com/company/neureal/" target="_blank"></a>
+      <a className='you-footer ion-social-youtube' href="https://www.youtube.com/channel/UCkebLOaWk9vplZ6ZWLcbvtA" target="_blank"></a>
+      <a href="https://t.co/UiTeDbNd2i"  target="_blank">
+            <img className='telegram-logo-footer' src={require( '../images/icons8-telegram-app-512.png')} alt="" />
+          </a>
+    </div>
   </div>
 </div>
 ) } }
