a781d4ee1eaa11a7ec648fa52d60488f67ae8093
Vincent Howard <vincenguyenhoward@gmail.com>
2018-03-15 15:42:58 -0600

    testing link 'https://sharp-neumann-d1ae35.netlify.com/#/' on master

--- static.config.js
+++ static.config.js
@@ -3,7 +3,7 @@
 import React, { Component } from 'react';
 
 export default {
-  siteRoot: 'https://quizzical-swartz-67de0c.netlify.com',
+  siteRoot: 'https://sharp-neumann-d1ae35.netlify.com/',
 
   getRoutes: async () => {
     return [
